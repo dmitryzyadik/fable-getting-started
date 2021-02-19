43d0abc3f65356f1dbe3e18c95cfebc3e53e390a
dmitryzyadik <zyadik@mail.ru>
2021-02-19 16:45:40 +0500

    add Cmd

--- src/App.fs
+++ src/App.fs
@@ -5,38 +5,35 @@ open Fable.React.Props
 open Elmish
 open Elmish.React
 
-type Model = 
-    {
-        Count : int
-    }
+type Model = { Count: int }
 
 type Msg =
     | Increment
     | Decrement
+    | IncrementTwice
 
-let init () = 
-    {
-     Count = 42
-    }, Cmd.none
+let init () = { Count = 42 }, Cmd.none
 
-let update msg model : Model * Cmd<Msg> =  
+let update msg model: Model * Cmd<Msg> =
     match msg with
-    | Increment ->
-        { model with Count = model.Count + 1 } , Cmd.none
-    | Decrement ->
-        { model with Count = model.Count - 1 } , Cmd.none
-   
-     
-let view model dispatch = 
-    div [] 
-        [
-         button [ OnClick (fun ev -> dispatch Decrement)] [ str "-"]
-         h1 [] [ ofInt model.Count]
-         button [ OnClick (fun ev -> dispatch Increment)][ str "+"]
+    | IncrementTwice -> 
+        model, Cmd.batch [Cmd.ofMsg Increment; Cmd.ofMsg Increment;]
+        
+    | Increment -> { model with Count = model.Count + 1 }, Cmd.none
+    | Decrement -> { model with Count = model.Count - 1 }, Cmd.none
+
+
+let view model dispatch =
+    div [] [
+        button [ OnClick(fun ev -> dispatch Decrement) ] [
+            str "-"
         ]
+        h1 [] [ ofInt model.Count ]
+        button [ OnClick(fun ev -> dispatch IncrementTwice) ] [
+            str "+"
+        ]
+    ]
 
 Program.mkProgram init update view
 |> Program.withReactSynchronous "app"
 |> Program.run
-
-
